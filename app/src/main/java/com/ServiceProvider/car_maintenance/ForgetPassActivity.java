966f0d89565992cf18bcec5b50a3ddb8c2dcb885
Muzammil Zafar <muzammilzafar19@gmail.com>
2019-03-14 15:14:30 +0500

    new upload

--- app/src/main/java/com/ServiceProvider/car_maintenance/ForgetPassActivity.java
+++ app/src/main/java/com/ServiceProvider/car_maintenance/ForgetPassActivity.java
@@ -2,14 +2,23 @@ package com.ServiceProvider.car_maintenance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class ForgetPassActivity extends AppCompatActivity {
-
+    private Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pass);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        btnback=findViewById(R.id.btn_back);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ServiceProvider/car_maintenance/RegistrationActivity.java
+++ app/src/main/java/com/ServiceProvider/car_maintenance/RegistrationActivity.java
@@ -1,15 +1,26 @@
 package com.ServiceProvider.car_maintenance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private Button sign_in_button;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        btnSignIn =  findViewById(R.id.sign_in_button);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
